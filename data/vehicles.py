dd699982da28012a9a3b79aaf8c32cc69587f252
Clara Carbo <claracarbocanals@gmail.com>
2021-01-05 11:56:09 +0100

    .gitignored modified and folder data

---
+++ data/vehicles.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+vehicles = pd.read_csv("~/Desktop/Data/Pandas/vehicles.csv")
+
+
+# In[3]:
+
+
+vehicles.shape
+
+
+# In[ ]:
+
+
+
+
