8fef8ecf62fd9f2691ed665f1e1f31b483ac52d8
Clara Carbo <claracarbocanals@gmail.com>
2021-01-07 19:44:11 +0100

    README and function updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# vehicles_insight
+
+#### Project 1
+
+###### Given a df, prints its shape, number of rows and columns, column names, min and max values on terminal.
--- data/Vehicles.py
+++ data/Vehicles.py
@@ -13,17 +13,82 @@ import pandas as pd
 vehicles = pd.read_csv("~/Desktop/Data/Pandas/vehicles.csv")
 
 
-# In[4]:
+# In[6]:
 
 
 def shape(x):
-    print(x.shape)
+    print("The shape of the dataframe is", x.shape)
 
 
-# In[5]:
+# In[7]:
 
 
 shape(vehicles)
+
+
+# In[8]:
+
+
+def rows(x):
+    print("It has a total of", len(x), "rows")
+
+
+# In[9]:
+
+
+rows(vehicles)
+
+
+# In[10]:
+
+
+def columns(x):
+    print("It has a total of", len(x.columns), "columns")
+
+
+# In[11]:
+
+
+columns(vehicles)
+
+
+# In[54]:
+
+
+def column_names(x):
+    print("Columns are:",list(x.columns))
+
+
+# In[52]:
+
+
+column_names(vehicles)
+
+
+# In[91]:
+
+
+def min_value(x):
+    print(x.min(axis=None, skipna=None, level=None, numeric_only=True))
+
+
+# In[92]:
+
+
+min_value(vehicles)
+
+
+# In[90]:
+
+
+def max_value(x):
+    print(x.max(axis=None, skipna=None, level=None, numeric_only=True))
+
+
+# In[93]:
+
+
+max_value(vehicles)
 
 
 # In[ ]:
